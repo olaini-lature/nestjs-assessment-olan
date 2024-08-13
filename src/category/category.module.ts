7e9c1d26de6b6ff4031d18fe6c87bd52071b24fb
Olaini Lature <144663136+olainilature@users.noreply.github.com>
2024-08-13 15:37:30 +0700

    add import passport module in category module for support auth guard

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -3,9 +3,13 @@ import { CategoryService } from './category.service';
 import { CategoryController } from './category.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './category.entity';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Category])],
+  imports: [
+    TypeOrmModule.forFeature([Category]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+  ],
   providers: [CategoryService],
   controllers: [CategoryController],
 })

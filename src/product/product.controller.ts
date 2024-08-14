e1c981122410e50ff110953d6ea30fc50dcab634
Olaini Lature <144663136+olainilature@users.noreply.github.com>
2024-08-14 10:30:20 +0700

    add: auth guard for product service

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post, UseGuards } from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('product')
+@UseGuards(AuthGuard())
 export class ProductController {
 
   private logger = new Logger('ProductController', { timestamp: true });
--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -4,9 +4,14 @@ import { ProductController } from './product.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product } from './product.entity';
 import { CategoryModule } from 'src/category/category.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product]), CategoryModule],
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    CategoryModule,
+  ],
   providers: [ProductService],
   controllers: [ProductController],
 })

5a506b05677029c1c0f7e5e6f9649fa8f8cb3314
Olaini Lature <144663136+olainilature@users.noreply.github.com>
2024-08-14 10:59:36 +0700

    update: create product service to be accessed only by admin

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,20 +1,40 @@
-import { Body, Controller, Logger, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  ForbiddenException,
+  Logger,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/shared/decorators/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 
 @Controller('product')
 @UseGuards(AuthGuard())
 export class ProductController {
-
   private logger = new Logger('ProductController', { timestamp: true });
 
-  constructor(
-    private productService: ProductService
-  ) {}
+  constructor(private productService: ProductService) {}
 
   @Post('/create')
-  addCategory(@Body() createProductDto: CreateProductDto): Promise<void> {
+  addCategory(
+    @Body() createProductDto: CreateProductDto,
+    @GetUser() user: User,
+  ): Promise<void> {
+    this.logger.verbose(`Checking user access: ${JSON.stringify(user)}`);
+
+    if (user.type !== 'admin') {
+      this.logger.error(
+        `User don't have enough permission to access this service`,
+      );
+      throw new ForbiddenException(
+        `User don't have enough permission to access this service`,
+      );
+    }
+
     this.logger.verbose(`Add product: ${JSON.stringify(createProductDto)}`);
     return this.productService.createProduct(createProductDto);
   }
